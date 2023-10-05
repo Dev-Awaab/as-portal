7424f30ace10bc7fb368bee7ff50d0a30337758e
Abdul-Kareem Abdul-Fattah <adeoyin2828@gmail.com>
2023-10-05 11:08:06 +0100

    total for each week sell order and buy order

--- frontend/src/lib/components/WeeklyFigure.svelte
+++ frontend/src/lib/components/WeeklyFigure.svelte
@@ -1,109 +1,3 @@
-<!-- <script lang="ts">
-	import {
-		Heading,
-		Table,
-		TableBody,
-		TableBodyCell,
-		TableBodyRow,
-		TableHead,
-		TableHeadCell
-	} from 'flowbite-svelte';
-	import { useTradeStore } from '../../store/weeklyFigure';
-	import { onMount } from 'svelte';
-	import axios from 'axios';
-	import { dataStore, fetchData } from '../../store/testStore';
-
-	let result: any = [];
-	onMount(async () => {
-		try {
-			const res = await axios.get(
-				'https://trade-accounting-demo.onrender.com/api/weeklyfigures/retrive'
-			);
-
-			result = res.data.data.data;
-		} catch (error) {
-			console.error('Error fetching data:', error);
-		}
-	});
-	console.log(result);
-
-
-	// Calculate the "Total" for each week
-	let weeklyTotalData = result?.map((item: any, index: number) => {
-		const total = (
-			parseInt(item.BUYORDER.replace(/\D/g, '')) + parseInt(item.SELLORDER.replace(/\D/g, ''))
-		).toLocaleString('en-NG', {
-			style: 'currency',
-			currency: 'NGN'
-		});
-		return { ...item, total };
-	});
-
-	// Add the "Total" row at the end
-	weeklyTotalData.push({
-		date: 'Total',
-		buyOrder: weeklyTotalData
-			.reduce(
-				(total: number, item: { BUYORDER: string }) =>
-					total + parseInt(item.BUYORDER.replace(/\D/g, '')),
-				0
-			)
-			.toLocaleString('en-NG', {
-				style: 'currency',
-				currency: 'NGN'
-			}),
-		sellOrder: weeklyTotalData
-			.reduce(
-				(total: number, item: { SELLORDER: string }) =>
-					total + parseInt(item.SELLORDER.replace(/\D/g, '')),
-				0
-			)
-			.toLocaleString('en-NG', {
-				style: 'currency',
-				currency: 'NGN'
-			}),
-		total: weeklyTotalData
-			.reduce(
-				(total: number, item: { total: string }) => total + parseInt(item.total.replace(/\D/g, '')),
-				0
-			)
-			.toLocaleString('en-NG', {
-				style: 'currency',
-				currency: 'NGN'
-			})
-	});
-
-	console.log(weeklyTotalData)
-
-
-</script>
-
-<div>
-	<div class="flex items-center py-5 px-3">
-		<Heading tag="h5">Transaction Volume</Heading>
-	</div>
-	<Table>
-		<TableHead defaultRow={false}>
-			<tr>
-				<TableHeadCell>Date</TableHeadCell>
-				<TableHeadCell>Buy Order</TableHeadCell>
-				<TableHeadCell>Sell Order</TableHeadCell>
-				<TableHeadCell>Total</TableHeadCell>
-			</tr>
-		</TableHead>
-		<TableBody class="divide-y">
-			{#each weeklyTotalData as item, index (index)}
-				<TableBodyRow>
-					<TableBodyCell>{item.date}</TableBodyCell>
-					<TableBodyCell>Buy: {item.BUYORDER}</TableBodyCell>
-					<TableBodyCell>Sell: {item.SELLORDER}</TableBodyCell>
-					<TableBodyCell>{item.total}</TableBodyCell>
-				</TableBodyRow>
-			{/each}
-		</TableBody>
-	</Table>
-</div> -->
-<!-- src/routes/index.svelte -->
 <script lang="ts">
 	import {
 		Heading,
@@ -178,6 +72,7 @@
 					<TableHeadCell>Date</TableHeadCell>
 					<TableHeadCell>Buy Order</TableHeadCell>
 					<TableHeadCell>Sell Order</TableHeadCell>
+					<TableHeadCell>Total</TableHeadCell>
 				</tr>
 			</TableHead>
 			<TableBody>
@@ -186,6 +81,7 @@
 						<TableBodyCell>{moment(item.DATE).format('D, MMM, YYYY')}</TableBodyCell>
 						<TableBodyCell>{num(item.BUYORDER)}</TableBodyCell>
 						<TableBodyCell>{num(item.SELLORDER)}</TableBodyCell>
+						<TableBodyCell>{num(item.SELLORDER + item.BUYORDER)}</TableBodyCell>
 					</TableBodyRow>
 				{/each}
 			</TableBody>
