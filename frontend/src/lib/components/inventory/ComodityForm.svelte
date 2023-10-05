136ae9b959bc54113ce4edea30cdf5aceabc29b8
Abdul-Kareem Abdul-Fattah <adeoyin2828@gmail.com>
2023-10-05 09:21:49 +0100

    inventory

---
+++ frontend/src/lib/components/inventory/ComodityForm.svelte
@@ -0,0 +1,70 @@
+<script>
+	import {
+		Heading,
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte';
+
+	/**
+	 * @type {any}
+	 */
+	export let comodityData;
+	/**
+	 * @type {any}
+	 */
+	export let comodityName;
+
+	// Sample data
+</script>
+
+<div>
+	<div class="flex items-center py-5 px-3">
+		<Heading tag="h5">{comodityName}</Heading>
+	</div>
+	<Table class="w-full">
+		<TableHead defaultRow={false} class="w-full">
+			<TableHeadCell />
+			<TableHeadCell>Volume</TableHeadCell>
+			<TableHeadCell>Mkt Price</TableHeadCell>
+			<TableHeadCell>Value</TableHeadCell>
+			<TableHeadCell />
+		</TableHead>
+		<TableBody>
+			{#each comodityData as item (item.label)}
+				<TableBodyRow>
+					<TableBodyCell>{item.label}</TableBodyCell>
+					<TableBodyCell>{item.volume}</TableBodyCell>
+					<TableBodyCell>{item.marketPrice}</TableBodyCell>
+					<TableBodyCell>{item.value}</TableBodyCell>
+				</TableBodyRow>
+			{/each}
+		</TableBody>
+	</Table>
+</div>
+
+<!-- <div class="">
+	<TableHeadCell>SMAZ</TableHeadCell>
+	<Table hoverable={true}>
+		<TableHead>
+			<TableHeadCell />
+			<TableHeadCell>Volume</TableHeadCell>
+			<TableHeadCell>Mkt Price</TableHeadCell>
+			<TableHeadCell>Value</TableHeadCell>
+			<TableHeadCell />
+		</TableHead>
+		<TableBody class="divide-y">
+			{#each data as item (item.label)}
+				<TableBodyRow>
+					<TableBodyCell>{item.label}</TableBodyCell>
+					<TableBodyCell>{item.volume}</TableBodyCell>
+					<TableBodyCell>{item.marketPrice}</TableBodyCell>
+					<TableBodyCell>{item.value}</TableBodyCell>
+				</TableBodyRow>
+			{/each}
+		</TableBody>
+	</Table>
+</div> -->
---
+++ frontend/src/routes/(dashboard)/inventory/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import ComodityForm from '$lib/components/inventory/ComodityForm.svelte';
+	import { scsnData, smazData, sprlData, ssbsData, ssgmData } from '$lib/components/inventory/data';
+</script>
+
+<div class="space-y-5">
+	<ComodityForm comodityData={smazData} comodityName="SMAZ" />
+	<ComodityForm comodityData={ssgmData} comodityName="SSGM" />
+	<ComodityForm comodityData={ssbsData} comodityName="SSBS" />
+	<ComodityForm comodityData={sprlData} comodityName="SPRL" />
+	<ComodityForm comodityData={scsnData} comodityName="SCSN" />
+</div>
